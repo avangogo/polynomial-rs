f88c414639b5b5e99b22aa5bf651a8a5e48b1958
Travis CI User <travis@example.org>
2018-08-16 01:07:23 +0000

    Automatic Travis documentation build

---
+++ master/aliases.js
@@ -0,0 +1,3 @@
+var ALIASES = {};
+ALIASES['num_traits'] = {};
+ALIASES['polynomial'] = {};
---
+++ master/implementors/core/marker/trait.Send.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["num_traits"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"num_traits/struct.ParseFloatError.html\" title=\"struct num_traits::ParseFloatError\">ParseFloatError</a>",synthetic:true,types:["num_traits::ParseFloatError"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"enum\" href=\"num_traits/enum.FloatErrorKind.html\" title=\"enum num_traits::FloatErrorKind\">FloatErrorKind</a>",synthetic:true,types:["num_traits::FloatErrorKind"]},];
+implementors["polynomial"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"polynomial/struct.Polynomial.html\" title=\"struct polynomial::Polynomial\">Polynomial</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a>,&nbsp;</span>",synthetic:true,types:["polynomial::Polynomial"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
