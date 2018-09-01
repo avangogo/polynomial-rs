bb3d8f699fda6c5a9a71e6122dfc7cc450b60cca
Travis CI User <travis@example.org>
2018-09-01 03:22:31 +0000

    Automatic Travis documentation build

--- master/aliases.js
+++ master/aliases.js
@@ -1,16 +1,5 @@
 var ALIASES = {};
 ALIASES['num_traits'] = {};
-ALIASES['num_traits'] = {};
-ALIASES['num_traits'] = {};
-ALIASES['num_traits'] = {};
-ALIASES['num_traits'] = {};
-ALIASES['num_traits'] = {};
-ALIASES['num_traits'] = {};
-ALIASES['num_traits'] = {};
-ALIASES['num_traits'] = {};
-ALIASES['num_traits'] = {};
-ALIASES['num_traits'] = {};
-ALIASES['num_traits'] = {};
 ALIASES['polynomial'] = {};
 ALIASES['polynomial'] = {};
 ALIASES['polynomial'] = {};
