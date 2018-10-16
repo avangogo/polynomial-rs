ea01a668c4ab17437c9af3541222828c908cd3d4
Travis CI User <travis@example.org>
2018-10-16 05:10:43 +0000

    Automatic Travis documentation build

--- master/implementors/core/cmp/trait.PartialEq.js
+++ master/implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["polynomial"] = [{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"polynomial/struct.Polynomial.html\" title=\"struct polynomial::Polynomial\">Polynomial</a>&lt;T&gt;",synthetic:false,types:["polynomial::Polynomial"]},];
+implementors["polynomial"] = [{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&lt;<a class=\"struct\" href=\"polynomial/struct.Polynomial.html\" title=\"struct polynomial::Polynomial\">Polynomial</a>&lt;T&gt;&gt; for <a class=\"struct\" href=\"polynomial/struct.Polynomial.html\" title=\"struct polynomial::Polynomial\">Polynomial</a>&lt;T&gt;",synthetic:false,types:["polynomial::Polynomial"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
