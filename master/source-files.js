a4398fef630e6e1822d4845dd70ea7753c1e9bfd
Travis CI User <travis@example.org>
2018-12-06 09:16:34 +0000

    Automatic Travis documentation build

---
+++ master/source-files.js
@@ -0,0 +1,5 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['num_traits'] = {"name":"","dirs":[{"name":"ops","dirs":[],"files":["checked.rs","inv.rs","mod.rs","mul_add.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]};
+sourcesIndex['num_traits'] = {"name":"","dirs":[{"name":"ops","dirs":[],"files":["checked.rs","inv.rs","mod.rs","mul_add.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]};
+sourcesIndex['polynomial'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['polynomial'] = {"name":"","dirs":[],"files":["lib.rs"]};
