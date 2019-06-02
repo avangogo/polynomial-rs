3b04932afeb4b852c3f66bebc56c4cc92baa1cdb
Travis CI User <travis@example.org>
2019-06-02 13:13:05 +0000

    Automatic Travis documentation build

--- master/source-files.js
+++ master/source-files.js
@@ -1,7 +1,14 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["num_traits"] = {"name":"","dirs":[{"name":"ops","dirs":[],"files":["checked.rs","inv.rs","mod.rs","mul_add.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]};
-sourcesIndex["polynomial"] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex["num_traits"] = {"name":"","dirs":[{"name":"ops","files":["checked.rs","inv.rs","mod.rs","mul_add.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]};
+sourcesIndex["polynomial"] = {"name":"","files":["lib.rs"]};
 sourcesIndex['num_traits'] = {"name":"","dirs":[{"name":"ops","dirs":[],"files":["checked.rs","inv.rs","mod.rs","mul_add.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]};
+sourcesIndex['num_traits'] = {"name":"","dirs":[{"name":"ops","dirs":[],"files":["checked.rs","inv.rs","mod.rs","mul_add.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]};
+sourcesIndex['num_traits'] = {"name":"","dirs":[{"name":"ops","dirs":[],"files":["checked.rs","inv.rs","mod.rs","mul_add.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]};
+sourcesIndex['num_traits'] = {"name":"","dirs":[{"name":"ops","dirs":[],"files":["checked.rs","inv.rs","mod.rs","mul_add.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]};
+sourcesIndex['num_traits'] = {"name":"","dirs":[{"name":"ops","dirs":[],"files":["checked.rs","inv.rs","mod.rs","mul_add.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]};
+sourcesIndex['num_traits'] = {"name":"","dirs":[{"name":"ops","dirs":[],"files":["checked.rs","inv.rs","mod.rs","mul_add.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]};
+sourcesIndex['num_traits'] = {"name":"","dirs":[{"name":"ops","dirs":[],"files":["checked.rs","inv.rs","mod.rs","mul_add.rs","saturating.rs","wrapping.rs"]}],"files":["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]};
+sourcesIndex['polynomial'] = {"name":"","dirs":[],"files":["lib.rs"]};
 sourcesIndex['polynomial'] = {"name":"","dirs":[],"files":["lib.rs"]};
 sourcesIndex['polynomial'] = {"name":"","dirs":[],"files":["lib.rs"]};
 sourcesIndex['polynomial'] = {"name":"","dirs":[],"files":["lib.rs"]};
